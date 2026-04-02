4d9496618a7c3b12afe322720aa3d7471e31dc33
Gerardo Garnica <gerardo.garnica@gmail.com>
2026-04-02 17:41:23 -0500

    Add centralized DI setup for application services

---
+++ src/Aurora.Flowboard.Application/DependencyInjection.cs
@@ -0,0 +1,72 @@
+using Aurora.Flowboard.Application.Abstractions.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aurora.Flowboard.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services) => services
+        .AddMessagingHandlers()
+        .AddBehaviors()
+        .AddDomainHandlers()
+        .AddDomainServices()
+        .AddValidatorsFromAssembly();
+
+    private static IServiceCollection AddMessagingHandlers(this IServiceCollection services)
+    {
+        services
+            .Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        services
+            .Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        services
+            .Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
+
+    private static IServiceCollection AddBehaviors(this IServiceCollection services)
+    {
+        services.Decorate(typeof(ICommandHandler<,>), typeof(ValidationBehavior.CommandHandler<,>));
+        services.Decorate(typeof(ICommandHandler<>), typeof(ValidationBehavior.CommandBaseHandler<>));
+
+        services.Decorate(typeof(IQueryHandler<,>), typeof(LoggingBehavior.QueryHandler<,>));
+        services.Decorate(typeof(ICommandHandler<,>), typeof(LoggingBehavior.CommandHandler<,>));
+        services.Decorate(typeof(ICommandHandler<>), typeof(LoggingBehavior.CommandBaseHandler<>));
+
+        return services;
+    }
+
+    private static IServiceCollection AddDomainHandlers(this IServiceCollection services)
+    {
+        services
+            .Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
+
+    private static IServiceCollection AddDomainServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    private static IServiceCollection AddValidatorsFromAssembly(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
+
+        return services;
+    }
+}
