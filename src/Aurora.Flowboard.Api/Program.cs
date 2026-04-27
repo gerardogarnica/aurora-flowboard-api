13ca8a1beb2d437528a3620298bfc5ab4dd03f02
Gerardo Garnica <gerardo.garnica@gmail.com>
2026-04-27 14:52:07 -0500

    Add API layer project

---
+++ src/Aurora.Flowboard.Api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+await app.RunAsync();
+
+// REMARK: Required for functional and integration tests to work.
+namespace Aurora.Flowboard.Api
+{
+    public partial class Program;
+}
