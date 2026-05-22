c7d142d2b507950e4b6c295ec3a4d1bad5407cc4
Gerardo Garnica <gerardo.garnica@gmail.com>
2026-05-22 15:40:25 -0500

    Add database migration extensions

---
+++ src/Aurora.Flowboard.Api/Extensions/MigrationServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Aurora.Flowboard.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aurora.Flowboard.Api.Extensions;
+
+internal static class MigrationServiceExtensions
+{
+    internal static async Task ApplyMigrationsAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        await using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+    }
+}
