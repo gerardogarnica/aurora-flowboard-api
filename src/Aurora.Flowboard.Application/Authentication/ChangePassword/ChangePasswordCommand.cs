5f7b3f0e91bddd5214cc3d7e417b4e1633c3d8f9
Gerardo Garnica <gerardo.garnica@gmail.com>
2026-08-14 19:24:43 -0500

    Redact sensitive data in command ToString() methods for ChangePassword and Login commands

--- src/Aurora.Flowboard.Application/Authentication/ChangePassword/ChangePasswordCommand.cs
+++ src/Aurora.Flowboard.Application/Authentication/ChangePassword/ChangePasswordCommand.cs
@@ -1,3 +1,6 @@
 namespace Aurora.Flowboard.Application.Authentication.ChangePassword;
 
-public sealed record ChangePasswordCommand(string CurrentPassword, string NewPassword) : ICommand;
+public sealed record ChangePasswordCommand(string CurrentPassword, string NewPassword) : ICommand
+{
+    public override string ToString() => $"{nameof(ChangePasswordCommand)} {{ Redacted }}";
+}
