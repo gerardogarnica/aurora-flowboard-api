f9ee396156b999a63da1f62074842271e1088373
Gerardo Garnica <gerardo.garnica@gmail.com>
2026-08-27 15:27:19 -0500

    Fix login command log format to avoid ambiguous field separator

--- src/Aurora.Flowboard.Application/Authentication/Login/LoginCommand.cs
+++ src/Aurora.Flowboard.Application/Authentication/Login/LoginCommand.cs
@@ -2,5 +2,5 @@ namespace Aurora.Flowboard.Application.Authentication.Login;
 
 public sealed record LoginCommand(string Email, string Password) : ICommand<IdentityToken>
 {
-    public override string ToString() => $"{nameof(LoginCommand)} {{ Email = {Email}, Password = Redacted }}";
+    public override string ToString() => $"{nameof(LoginCommand)} {{ Email = {Email}, Password: [REDACTED] }}";
 }
